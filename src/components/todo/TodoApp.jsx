e6ba25b5dc91afe450c48f51b62875064dee6d53
avinash273 <avinash.shanker@mavs.uta.edu>
2021-03-28 12:32:17 -0500

    controlled component in React

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,18 +5,48 @@ class TodoApp extends Component {
         return (
             <div>
                 Todo Application
-                <LoginComponent />
+                <LoginComponent/>
             </div>
         );
     }
 }
 
 class LoginComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'avinash',
+            password: ''
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        console.log("username: " + event.target.value);
+        this.setState({username: event.target.value})
+    }
+
+    handlePasswordChange(event) {
+        console.log("password: " + event.target.value);
+        this.setState({password: event.target.value})
+    }
+
     render() {
         return (
             <div>
-                Username: <input type="text" name="username"/>
-                Password: <input type="password" name="password"/>
+                Username: <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleUsernameChange}
+            />
+                Password: <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+            />
                 <button>Login</button>
             </div>
         );
