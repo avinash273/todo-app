f3df04143bd01c9bb055c0fc7612034383df1fc2
avinash273 <avinash.shanker@mavs.uta.edu>
2021-03-28 12:06:46 -0500

    Todo app start

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { Component} from "react";
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import Counter from "./components/counter/counter";
+import TodoApp from "./components/todo/TodoApp";
 
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-          <Counter />
+          {/*<Counter />*/}
+          <TodoApp />
       </div>
     );
   }
