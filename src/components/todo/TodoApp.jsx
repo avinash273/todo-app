011147cae918653da980de09b0dbd0ea0994dfc6
avinash273 <avinash.shanker@mavs.uta.edu>
2021-03-28 12:10:59 -0500

    Login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,7 +4,20 @@ class TodoApp extends Component {
     render() {
         return (
             <div>
-                Todo App
+                Todo Application
+                <LoginComponent />
+            </div>
+        );
+    }
+}
+
+class LoginComponent extends Component {
+    render() {
+        return (
+            <div>
+                Username: <input type="text" name="username"/>
+                Password: <input type="password" name="password"/>
+                <button>Login</button>
             </div>
         );
     }
